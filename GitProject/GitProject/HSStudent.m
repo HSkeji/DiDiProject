c523edf7258690fae73a688a6b45b37648b80da4
HSkeji <huashanede@163.com>
2018-10-19 16:21:19 +0800

    Test1增加了Student类

---
+++ GitProject/GitProject/HSStudent.m
@@ -0,0 +1,13 @@
+//
+//  HSStudent.m
+//  GitProject
+//
+//  Created by 王泽 on 2018/10/19.
+//  Copyright © 2018年 Huashankeji. All rights reserved.
+//
+
+#import "HSStudent.h"
+
+@implementation HSStudent
+
+@end
