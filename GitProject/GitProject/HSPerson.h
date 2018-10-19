63afbcdbc5ae7f88966a7c02e2911257dbbcb936
HSkeji <huashanede@163.com>
2018-10-19 15:45:28 +0800

    增加了一个类，并有一个打印方法

---
+++ GitProject/GitProject/HSPerson.h
@@ -0,0 +1,15 @@
+//
+//  HSPerson.h
+//  GitProject
+//
+//  Created by 王泽 on 2018/10/19.
+//  Copyright © 2018年 Huashankeji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HSPerson : NSObject
+
+@property (nonatomic, strong) NSString *nameStr;
+
+@end
